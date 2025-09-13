ac1324487ea76e588b91173400bd23c55eb5b56e
Marconic12 <juanpodercasadeluz@gmail.com>
2025-09-13 17:51:31 -0600

    Implementacion de iconos y estilos

---
+++ lib/core/app_color.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+
+class AppColor {
+  // Background
+  static const Color primary = Color.fromRGBO(152, 251, 152, 1);
+  static const Color secondary = Color.fromARGB(235, 157, 29, 27);
+  static const Color fondito =  Color.fromARGB(235, 79, 187, 241);
+
+  //Buttons
+
+  static const Color button_primario = Color.fromARGB(255, 235, 226, 226);
+}
